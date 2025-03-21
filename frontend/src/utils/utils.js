7aef8d4afad11c96b4e1901e894cae43c25a9afc
Krishna Bansal <krishna.web@gmail.com>
2025-03-21 09:40:25 +0530

    update baseURL

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 export const axiosClient = axios.create({
-    baseURL:'http://localhost:1234/api/v1', 
+    // baseURL:'http://localhost:1234/api/v1', 
+    baseURL:'https://mern-stack-5rwh.onrender.com/api/v1'
 
 })
