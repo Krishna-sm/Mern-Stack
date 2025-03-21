6203b07326d1a9738515230675d9ae18c33089f8
Krishna Bansal <krishna.web@gmail.com>
2025-03-21 09:32:16 +0530

    initial commit

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 export const axiosClient = axios.create({
-    baseURL:'/api/v1',
-    //for development: http://localhost:1234/api/v1
+    baseURL:'http://localhost:1234/api/v1', 
 
 })
--- full_stack/contactModel.js
+++
@@ -1,9 +0,0 @@
-const { default: mongoose } = require("mongoose");
-
-const schema = new mongoose.Schema({
-    name:String,
-    email:String,
-    message:String,
-    subject:String
-})
-module.exports = mongoose.model("contact",schema)
